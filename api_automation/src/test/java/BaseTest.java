47011ee49241e42ffa4afa12af431587f64ae4c1
MohammadKamran <mrmohd012@gmail.com>
2026-08-31 12:50:07 +0530

    Make API base URL configurable

--- api_automation/src/test/java/BaseTest.java
+++ api_automation/src/test/java/BaseTest.java
@@ -7,8 +7,13 @@ public class BaseTest {
 
     public BaseTest() {
 
+        String baseUrl = System.getProperty(
+                "baseUrl",
+                "http://localhost:4000"
+        );
+
         requestSpecification = new RequestSpecBuilder()
-                .setBaseUri("http://localhost:4000")
+                .setBaseUri(baseUrl)
                 .setBasePath("/api/v1")
                 .setContentType("application/json")
                 .build();
